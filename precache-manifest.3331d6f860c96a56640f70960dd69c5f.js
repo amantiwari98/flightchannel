6c80f03aad77728adbb3a0356880234c195027e7
aman tiwari <amantiwari.kanpur20@gmail.com>
2021-01-28 17:15:41 +0530

    Updates

---
+++ precache-manifest.3331d6f860c96a56640f70960dd69c5f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5759d2e0de31432f1a12e843611d68c1",
+    "url": "/flightchannel/index.html"
+  },
+  {
+    "revision": "fca00a09f8dad48bfe4c",
+    "url": "/flightchannel/static/css/main.6dc39311.chunk.css"
+  },
+  {
+    "revision": "23e005250b5902d58c58",
+    "url": "/flightchannel/static/js/2.4600ec24.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/flightchannel/static/js/2.4600ec24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca00a09f8dad48bfe4c",
+    "url": "/flightchannel/static/js/main.18e255c0.chunk.js"
+  },
+  {
+    "revision": "3dde72e1dbc8c212d948",
+    "url": "/flightchannel/static/js/runtime-main.42670a2c.js"
+  },
+  {
+    "revision": "22be1ab72bcd08759663b44f8001f262",
+    "url": "/flightchannel/static/media/ams.22be1ab7.webp"
+  },
+  {
+    "revision": "abb28f6ffad1211522dfaeda643cbfcf",
+    "url": "/flightchannel/static/media/atlanta.abb28f6f.webp"
+  },
+  {
+    "revision": "438679baa66892c8fcda9d4a753bf0c9",
+    "url": "/flightchannel/static/media/beach.438679ba.webp"
+  },
+  {
+    "revision": "1efe539701d9c81e0ecb2764c001b823",
+    "url": "/flightchannel/static/media/bridge.1efe5397.webp"
+  },
+  {
+    "revision": "e22196d4da6f954a8edc1309381d6968",
+    "url": "/flightchannel/static/media/canada.e22196d4.webp"
+  },
+  {
+    "revision": "db735450ec2b17c820e26902bfef5356",
+    "url": "/flightchannel/static/media/cancun.db735450.webp"
+  },
+  {
+    "revision": "aaee3a1a87c9711b020102e65df4318b",
+    "url": "/flightchannel/static/media/city.aaee3a1a.webp"
+  },
+  {
+    "revision": "e6aa8ce1d69b231611dc69c5f5493400",
+    "url": "/flightchannel/static/media/globe.e6aa8ce1.webp"
+  },
+  {
+    "revision": "e6aa8ce1d69b231611dc69c5f5493400",
+    "url": "/flightchannel/static/media/globe2.e6aa8ce1.webp"
+  },
+  {
+    "revision": "d9f1a36f1d3c1b062193091fed947708",
+    "url": "/flightchannel/static/media/london.d9f1a36f.webp"
+  },
+  {
+    "revision": "8ee5984aa4a77d9c89c9ebb609261545",
+    "url": "/flightchannel/static/media/miami.8ee5984a.webp"
+  },
+  {
+    "revision": "7a7923ecdfa9fad1282267b51becc751",
+    "url": "/flightchannel/static/media/newyork.7a7923ec.webp"
+  },
+  {
+    "revision": "e74a28add83f5ed00e04945b3bc36f9c",
+    "url": "/flightchannel/static/media/paris.e74a28ad.webp"
+  },
+  {
+    "revision": "1ef201cb802a3f772e4511f8d2fcbbc1",
+    "url": "/flightchannel/static/media/pay.1ef201cb.png"
+  },
+  {
+    "revision": "1b604eb50d3a3e0f439665b2414c26db",
+    "url": "/flightchannel/static/media/punta.1b604eb5.webp"
+  },
+  {
+    "revision": "4c7c01d586e03e84fa240d2da4b44597",
+    "url": "/flightchannel/static/media/santo.4c7c01d5.webp"
+  },
+  {
+    "revision": "df82af6dc35b5ecebd984f1a228bc269",
+    "url": "/flightchannel/static/media/tour.df82af6d.jpeg"
+  },
+  {
+    "revision": "21d73a7533f6070bf6043b8277285c10",
+    "url": "/flightchannel/static/media/valarta.21d73a75.jpeg"
+  },
+  {
+    "revision": "7e6d861ddf7d72306ef71a2e3de31eb1",
+    "url": "/flightchannel/static/media/wall.7e6d861d.jpeg"
+  }
+]);
